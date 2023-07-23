93ad9b46a281177cc003ce858d5eea479c9660d7
SamuelTheophilus <ansahsamueltheophilus>
2023-07-23 18:45:54 +0000

    Added a simple comment

--- service.py
+++ service.py
@@ -5,7 +5,7 @@ model_ref = bentoml.xgboost.get("credit_risk_pred_model:latest")
 dv = model_ref.custom_objects["DictVectorizer"]
 
 model_runner = model_ref.to_runner()
-
+# Instantiate a service for the model_runner
 svc = bentoml.Service("credit_risk_classifier", runners= [model_runner])
 
 @svc.api(input=JSON(), output=JSON())
