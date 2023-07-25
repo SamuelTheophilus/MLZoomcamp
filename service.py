523cea0ea5980d6ab6057b7b80190b7e0b279c16
Silas-Asamoah <silasbempong@gmail.com>
2023-07-25 09:02:15 +0000

    feat: handle possible missing features

--- service.py
+++ service.py
@@ -10,15 +10,33 @@ model_runner = model_ref.to_runner()
 # Instantiate a service for the model_runner
 svc = bentoml.Service("credit_risk_classifier", runners=[model_runner])
 
-
 @svc.api(input=JSON(), output=JSON())
 def classify(application_data):
     try:
+        # List of all required features
+        required_features = ['age', 'amount', 'assets', 'debt', 'expenses', 'home=ignore',
+                             'home=other', 'home=owner', 'home=parents', 'home=private',
+                             'home=rent', 'home=unk', 'income', 'job=fixed', 'job=freelance',
+                             'job=others', 'job=partime', 'job=unk', 'marital=divorced',
+                             'marital=married', 'marital=separated', 'marital=single',
+                             'marital=widow', 'price', 'records=no', 'records=yes', 'seniority',
+                             'time']
+        
+        # Check if each feature is in application_data and if not, add it with a default value
+        for feature in required_features:
+            if feature not in application_data:
+                # If the feature is categorical (contains '='), use 'unknown' as the default value
+                if '=' in feature:
+                    application_data[feature] = 'unknown'
+                # Otherwise, it's numeric, so use 0 as the default value
+                else:
+                    application_data[feature] = 0
+
+        # Continue with the original prediction code
         vector = dv.transform(application_data)
         logging.info(f"This is the vector {vector}")
         prediction = model_runner.predict.run(vector)
         logging.info(prediction)
-        # Determine the status based on the prediction
         status = "Approved" if prediction == 1 else "Rejected"
         return {"status": status}
     except Exception as e:
