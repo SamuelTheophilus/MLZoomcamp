d092009e7de711b7149dd64acd673ed9ce31c155
Silas-Asamoah <silasbempong@gmail.com>
2023-07-25 08:56:57 +0000

    refactor: add error handling for service.py

--- service.py
+++ service.py
@@ -1,3 +1,5 @@
+import logging
+
 import bentoml
 from bentoml.io import JSON
 
@@ -6,13 +8,19 @@ dv = model_ref.custom_objects["DictVectorizer"]
 
 model_runner = model_ref.to_runner()
 # Instantiate a service for the model_runner
-svc = bentoml.Service("credit_risk_classifier", runners= [model_runner])
+svc = bentoml.Service("credit_risk_classifier", runners=[model_runner])
+
 
 @svc.api(input=JSON(), output=JSON())
 def classify(application_data):
-    vector = dv.transform(application_data)
-    print(f"This is the vector {vector}")
-    prediction = model_runner.predict.run(vector)
-    print(prediction)
-    return {"status": "Approved"}
-    
+    try:
+        vector = dv.transform(application_data)
+        logging.info(f"This is the vector {vector}")
+        prediction = model_runner.predict.run(vector)
+        logging.info(prediction)
+        # Determine the status based on the prediction
+        status = "Approved" if prediction == 1 else "Rejected"
+        return {"status": status}
+    except Exception as e:
+        logging.error("Error occurred: ", e)
+        return {"status": "Error occurred during prediction"}
